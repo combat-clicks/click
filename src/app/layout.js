20ed2eaae1f644dba908c62c178d31d36151ad5d
Pro-gram <stevenjeffers@live.ca>
2025-01-18 19:40:15 -0800

    Update gradient colors to light blue and black theme

---
+++ src/app/layout.js
@@ -0,0 +1,12 @@
+import '@/styles/tailwind.css'
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className="h-full">
+      <head>
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+      </head>
+      <body className="h-full">{children}</body>
+    </html>
+  )
+} 
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/app/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
